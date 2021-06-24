d7869a8fe21b7bb96682bb7e3456402b1c6bdb55
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-06-24 16:46:20 -0300

    feat: create proprietario model

---
+++ src/models/proprietario.model.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+import connect from '../repositories/db.js';
+
+const Proprietario = connect.define('proprietarios', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    telefone: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, { underscored: true });
+
+export {
+    Proprietario
+}
