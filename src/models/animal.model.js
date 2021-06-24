86304776fbd383826dc2376abc9d85eff0f6ef57
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-06-24 16:59:08 -0300

    feat: create animal model

---
+++ src/models/animal.model.js
@@ -0,0 +1,26 @@
+import Sequelize from "sequelize";
+import connect from '../repositories/db.js';
+import Proprietario from './proprietario.model.js';
+
+const Animal = connect.define('animais', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tipo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, { underscored: true });
+
+Animal.belongsTo(Proprietario, { foreignKey: 'proprietariosId' });
+
+export {
+    Animal
+}
