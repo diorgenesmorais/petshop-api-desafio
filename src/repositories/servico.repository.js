5d64ed7e126e6b23eca047fb06e5d8c7045d279a
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-06-27 12:43:45 -0300

    feat: create servico repository

---
+++ src/repositories/servico.repository.js
@@ -0,0 +1,83 @@
+import Servico from '../models/servico.model.js';
+import Animal from '../models/animal.model.js';
+
+const getAllAnimais = async () => {
+    try {
+        return await Servico.findAll({
+            include: [
+                {
+                    model: Animal
+                }
+            ]
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getByIdServico = async (id) => {
+    try {
+        return await Servico.findByPk(id);
+    } catch (error) {
+        throw error;
+    }
+}
+
+const createServico = async (servico) => {
+    try {
+        return await Servico.create(servico);
+    } catch (error) {
+        throw error;
+    }
+}
+
+const updateServico = async (servico) => {
+    try {
+        await Servico.update(servico, {
+            where:{
+                id: servico.id
+            }
+        });
+        return await getByIdServico(servico.id);
+    } catch (error) {
+        throw error;
+    }
+}
+
+const deleteServico = async (id) => {
+    try {
+        return await Servico.destroy({
+            where: {
+                id: id
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getByProprietario = async (proprietarioId) => {
+    try {
+        return await Servico.findAll({
+            include: [
+                {
+                    model: Animal,
+                    where: {
+                        proprietariosId
+                    }
+                }
+            ]
+        })
+    } catch (error) {
+        throw error;
+    }
+}
+
+export {
+    getAllAnimais,
+    getByIdServico,
+    createServico,
+    updateServico,
+    deleteServico,
+    getByProprietario
+}
