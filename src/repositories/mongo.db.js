5d01e3472ef3c784a813489cd6a48ff1fcc89017
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-06-28 11:51:29 -0300

    refactor: remove warning useUnifiedTopology

--- src/repositories/mongo.db.js
+++ src/repositories/mongo.db.js
@@ -1,5 +1,5 @@
 import mongodb from 'mongodb';
 
 export function getClient() {
-    return new mongodb.MongoClient(process.env.DB_MONGO);
+    return new mongodb.MongoClient(process.env.DB_MONGO, {useUnifiedTopology: true});
 }
